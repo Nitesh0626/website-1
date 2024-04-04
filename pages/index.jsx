adff3f0ea8f6b356415976030a4bd93357213c94
Nitesh0626 <nitesh.kmr0612@gmail.com>
2024-04-04 11:22:49 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
-          <motion.h1
+          <motion.h2
             variants={fadeIn("down", 0.2)}
             initial="hidden"
             animate="show"
@@ -24,7 +24,7 @@ const Home = () => {
           >
             Transforming Ideas <br /> Into{" "}
             <span className="text-accent">Digital Reality</span>
-          </motion.h1>
+          </motion.h2>
 
           {/* subtitle */}
           <motion.p
