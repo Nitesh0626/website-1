d1f1b7508681b7c86b4518888882bdb4987529bc
Nitesh0626 <nitesh.kmr0612@gmail.com>
2024-05-01 14:22:39 +0545

    Update index.jsx

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -29,9 +29,9 @@ const Contact = () => {
   return (
     <div className="h-full bg-primary/30">
       <div className="container mx-auto py-32 text-center xl:text-left flex items-center justify-center h-full">
-        {/* text & form */}
+        {/* Text & form */}
         <div className="flex flex-col w-full max-w-[700px]">
-          {/* text */}
+          {/* Text */}
           <motion.h2
             variants={fadeIn("up", 0.2)}
             initial="hidden"
@@ -42,7 +42,7 @@ const Contact = () => {
             Let's <span className="text-accent">connect.</span>
           </motion.h2>
 
-          {/* form */}
+          {/* Form */}
           <motion.form
             variants={fadeIn("up", 0.4)}
             initial="hidden"
@@ -55,28 +55,27 @@ const Contact = () => {
             // only needed for production (in netlify) to accept form input
             data-netlify="true"
           >
-            {/* input group */}
+            {/* Input group */}
             <div className="flex gap-x-6 w-full">
-            <input
-             type="text"
-             name="name"
-             placeholder="Name"
-              className="input"
-              // Only disable when submitting
-              disabled={isLoading === true} 
-              aria-disabled={isLoading === true}
-              required
-              aria-required
-            />
-
-           
+              <input
+                type="text"
+                name="name"
+                placeholder="Name"
+                className="input"
+                // Only disable when submitting
+                disabled={isLoading}
+                aria-disabled={isLoading}
+                required
+                aria-required
+              />
               <input
                 type="email"
                 name="email"
                 placeholder="E-mail"
                 className="input"
-                disabled={isLoading === true}
-                aria-disabled={isLoading === true}
+                // Only disable when submitting
+                disabled={isLoading}
+                aria-disabled={isLoading}
                 required
                 aria-required
               />
@@ -86,8 +85,9 @@ const Contact = () => {
               name="subject"
               placeholder="Subject"
               className="input"
-              disabled={isLoading === true}
-              aria-disabled={isLoading === true}
+              // Only disable when submitting
+              disabled={isLoading}
+              aria-disabled={isLoading}
               required
               aria-required
             />
@@ -95,30 +95,25 @@ const Contact = () => {
               name="message"
               placeholder="Message..."
               className="textarea"
-              disabled={isLoading === true}
-              aria-disabled={isLoading === true}
+              // Only disable when submitting
+              disabled={isLoading}
+              aria-disabled={isLoading}
               required
               aria-required
             />
             <button
-  type="submit"
-  className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
-  // Only disable submit functionality
-  disabled={isLoading}
-  aria-disabled={isLoading}
->
-  <span className="transition-all duration-500">  {/* Remove group-hover related styles */}
-    Let's talk
-  </span>
-
-  <BsArrowRight
-    className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]"
-    aria-hidden
-  />
-</button>
-
-            {/* particles */}
-        <ParticlesContainer />
+              type="submit"
+              className="btn rounded-full border border-white/50 max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
+              // Only disable submit functionality
+              disabled={isLoading}
+              aria-disabled={isLoading}
+            >
+              <span className="transition-all duration-500">Let's talk</span>
+              <BsArrowRight
+                className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]"
+                aria-hidden
+              />
+            </button>
           </motion.form>
         </div>
       </div>
